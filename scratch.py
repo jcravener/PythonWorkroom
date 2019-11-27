be22203f36484b7038b2611fab0b6658f546099d
John Cravener <johncrav@microsoft.com>
2019-11-27 11:34:46 -0800

    Factorial recursive

--- scratch.py
+++ scratch.py
@@ -2,12 +2,19 @@
 
 
 
-l = [1,2,3,4,5]
-print(l[-1])
 
-l.pop()
-print(l)
 
-l.insert(0,0)
-print(l)
+
+def nfactorial(n: int):
+
+    if n == 0:
+        return None
+    
+    if n == 1:
+        return 1
+    else:
+        return n * nfactorial(n-1)
+
+print(nfactorial(5))
+print(1*2*3*4*5)
 
