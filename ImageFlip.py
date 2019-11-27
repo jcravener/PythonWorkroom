dbc10ca582914d9f66e3abb1ddff344a8c8188c0
John Cravener <johncrav@microsoft.com>
2019-11-27 11:58:18 -0800

    Version 0.1 - note done yet

---
+++ ImageFlip.py
@@ -0,0 +1,15 @@
+
+
+
+def FlipAndInvertImage(A):
+
+    result = []
+    
+    if 1 not in A:
+        return(A)
+    elif 0 not in A:
+        return(A)
+    
+
+
+print(FlipAndInvertImage([1,1,1]))
