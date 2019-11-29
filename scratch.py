e2e4231dab958a53d1f513118fdda572331fd532
John Cravener <johncrav@microsoft.com>
2019-11-29 11:49:01 -0800

    first checking from VS code github int on SAW

--- scratch.py
+++ scratch.py
@@ -1,10 +1,4 @@
 
-
-
-
-
-
-
 def nfactorial(n: int):
 
     if n == 0:
@@ -17,4 +11,14 @@ def nfactorial(n: int):
 
 print(nfactorial(5))
 print(1*2*3*4*5)
+
+def fib(n):
+
+    if n <= 2:
+        return 1
+    else:
+        return fib(n-2) + fib(n-1)
+
+
+for n in range(10): print(fib(n))
 
