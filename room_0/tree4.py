1dc0f553d568a2762b6535b66ea85c0a1ef98a95
jcravener <jcravener@hotmail.com>
2019-12-16 19:51:23 -0800

    tree experiments

--- room_0/tree4.py
+++ room_0/tree4.py
@@ -8,13 +8,56 @@ class node:
         self.right = None
 
 def preorder_traverse(root: node):
+    l = []
+    
+    if root:
+        l.append(root.data)
+        l = l + preorder_traverse(root.left)
+        l = l + preorder_traverse(root.right)
+    
+    return l
 
-    print(root.data)
+def postorder_traverse(root: node):
+    l = []
+    
+    if root:
+        l = postorder_traverse(root.left)
+        l = l + postorder_traverse(root.right)
+        l.append(root.data)
 
-    if root.left:
-        return preorder_traverse(root.left)
-    if root.right:
-        return preorder_traverse(root.right)
+    return l
+
+def inorder_traverse(root: node):
+    l = []
+    
+    if root:
+        l = inorder_traverse(root.left)
+        l.append(root.data)
+        l = l + inorder_traverse(root.right)
+
+    return l
+
+def increasingtree(root: node):
+
+    l = inorder_traverse(root)
+    
+    cur_node = node(l[0])
+    rt = cur_node
+    
+    for d in l[1:]:
+        cur_node.right = node(d)
+        cur_node = cur_node.right
+    
+    return rt
+
+
+def inorder_display(root: node):
+
+    if root:
+        inorder_display(root.left)
+        print(root.data)
+        inorder_display(root.right)
+    return None
 
 def bfs(root: node):
     q = []
@@ -32,18 +75,25 @@ def bfs(root: node):
     
     return r
 
-l = node(8)
-r = node(12)
 
-rt = node(10)
-rt.left = l
-rt.right = r
+rt = node(5)
+rt.left = node(3)
+rt.right = node(6)
 
-ll = node(9)
-lr = node(6)
+(rt.left).left = node(2)
+(rt.left).right = node(4)
+((rt.left).left).left = node(1)
 
-l.left  = ll
-l.right = lr
+(rt.right).right = node(8)
+((rt.right).right).left = node(7)
+((rt.right).right).right = node(9)
 
-preorder_traverse(rt)
-print(bfs(rt))
+print(preorder_traverse(rt))
+print(postorder_traverse(rt))
+print(inorder_traverse(rt))
+
+newrt = increasingtree(rt)
+
+print(inorder_traverse(newrt))
+
+#print(bfs(rt))
