a0515d9a87dc38cf719072135245f2cdaf919d43
jcravener <jcravener@hotmail.com>
2020-01-07 21:51:47 -0800

    LeetCode 1022:  Iterative approach

--- SumofRootToLeafBinNum.py
+++ SumofRootToLeafBinNum.py
@@ -4,11 +4,14 @@ class TreeNode:
         self.left = None
         self.right = None
 
-def inorderdfs(r: TreeNode):
+def btreepath(r: TreeNode):
+
     if r == None:
         return None
-    
-    s = [[r, ""]]  #--- going to store each path as a string of vals with the traversal.
+
+    s = [[r, ""]]
+    base = "0b"
+    ct = 0
 
     while s:
         cur_node, path = s.pop()
@@ -20,8 +23,10 @@ def inorderdfs(r: TreeNode):
         
         if cur_node.left == None and cur_node.right == None:
             path += str(cur_node.val)
-            print(path)
-
+            ct += int(base + path, 2)
+    
+    return ct
+    
 r = TreeNode(1)
 r.left = TreeNode(0)
 r.right = TreeNode(1)
@@ -30,6 +35,6 @@ r.left.right = TreeNode(1)
 r.right.left = TreeNode(0)
 r.right.right = TreeNode(1)
 
-print(inorderdfs(r))
+print(btreepath(r))
 
 
