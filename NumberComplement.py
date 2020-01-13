2baab6e02d54d78d55a7e7f425bc144905db4cbe
jcravener <jcravener@hotmail.com>
2020-01-12 16:49:39 -0800

    removed prepending '0b' to complment string.

--- NumberComplement.py
+++ NumberComplement.py
@@ -1,3 +1,6 @@
+
+# LeetCode: 476. Number Complement
+
 def findComplement(num: int) -> int:
 
     bn = bin(num)
@@ -6,7 +9,7 @@ def findComplement(num: int) -> int:
     for b in bn[2:]:
         if b == '0': cn += '1'
         else: cn += '0'
-    cn = '0b' + cn
+    #cn = '0b' + cn
 
 
     return int(cn, 2)
