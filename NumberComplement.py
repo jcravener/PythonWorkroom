df3e37691061dd3f9f3d0777bac110487eafa124
jcravener <jcravener@hotmail.com>
2020-01-12 16:46:32 -0800

    LeetCode: 476. Number Complement

---
+++ NumberComplement.py
@@ -0,0 +1,15 @@
+def findComplement(num: int) -> int:
+
+    bn = bin(num)
+    cn = ''
+
+    for b in bn[2:]:
+        if b == '0': cn += '1'
+        else: cn += '0'
+    cn = '0b' + cn
+
+
+    return int(cn, 2)
+
+print(findComplement(5))
+print(findComplement(1))
