b57edcf24ee3f2e919f798e11bc3bbdd74050c64
jcravener <jcravener@hotmail.com>
2020-01-15 23:05:08 -0800

    Using list count method instead of dictionary.

--- UncommonWordsfromTwoSentences.py
+++ UncommonWordsfromTwoSentences.py
@@ -1,18 +1,21 @@
 
 
 # LeetCode: 884. Uncommon Words from Two Sentences
+# My first solution used a dictionary, and was only 89% fast.
+# This one uses the list count method and did not prove
+# faster per multiple submissions. However, the speed rating
+# kept varying widely (?). This one uses less code than the first.
 
 def uncommonFromSentences(A: str, B: str) -> [str]:
 
-    d = {}
+    a = A.split(' ') + B.split(' ')
+    l = []
 
-    for i in ( A.split(' ') + B.split(' ') ):
-        if i not in d:
-            d[i] = 1
-        else:
-            d[i] += 1
+    for i in a:
+        if a.count(i) == 1:
+            l.append(i)
 
-    return [k for k, v in d.items() if v == 1]
+    return l
 
 A = "this apple is sweet"
 B = "this apple is sour"
