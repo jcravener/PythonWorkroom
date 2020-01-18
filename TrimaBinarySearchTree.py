3c4dee02a8af593347192eecba8e30fedba28d50
jcravener <jcravener@hotmail.com>
2020-01-18 10:59:40 -0800

    Added depth finder function

--- TrimaBinarySearchTree.py
+++ TrimaBinarySearchTree.py
@@ -38,6 +38,29 @@ def insertBstNode(rt:TreeNode, v: int) -> TreeNode:
         else:
             rt.right = nnode
 
+def BSFdepth(rt:TreeNode):
+    if not rt:
+        return None
+    
+    q = [rt]
+    depth = 0
+    da = []
+
+    while q:
+        depth += 1
+        dq = []
+
+        for n in q:
+            if n.left:
+                dq.append(n.left)
+            if n.right:
+                dq.append(n.right)
+        
+        da.append([i.val for i in dq])
+        q = dq
+    
+    return depth, da
+
 def midlist(a:[int]) -> int:
     return len(a)//2
 
@@ -50,6 +73,8 @@ h.right = TreeNode(2)
 
 a = [i for i in inorderDfsTravers(h) if i >= 1 and i <=  2]
 print(a)
+print(BSFdepth(h))
+print()
 
 h = TreeNode(3)
 h.left = TreeNode(0)
@@ -58,4 +83,15 @@ h.right = TreeNode(4)
 ((h.left).right).left = TreeNode(1)
 a = [i for i in inorderDfsTravers(h) if i >= 1 and i <=  3]
 print(a)
+print(BSFdepth(h))
+print()
 
+a = [i for i in range(20)]
+r = TreeNode(a[midlist(a)])
+print(inorderDfsTravers(r))
+
+for e in a:
+    insertBstNode(r,e)
+
+print(inorderDfsTravers(r))
+print(BSFdepth(r))
