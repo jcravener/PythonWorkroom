aca385b9b867cca7def520c22cf8a17feaa08cf2
jcravener <jcravener@hotmail.com>
2020-01-26 12:08:23 -0800

    Iterative solution

--- LetterCasePermutation.py
+++ LetterCasePermutation.py
@@ -5,17 +5,22 @@
 dp = {}
 
 def letterCasePermutation(S: str) -> [str]:
-    
-    l = []
 
-    if len(S) == 1:
-        return S
-    else:
-        for i in range(len(S)):
-            for p in letterCasePermutation(S[:i] + S[i+1:]):
-                l.append(S[i] + p)
-    
-    return l
+    perm = [S]  #-- seed perm list with input string
+
+    #-- iterate through each char in input
+    for i in range(len(S)):
+        #-- we're only working on alpha chars
+        if S[i].isalpha():
+            #-- now, we're going to work on only the existing str in the perm list
+            p_len = len(perm)
+            #--- for each str in per, swap case of char at the original possition
+            for j in range(p_len):
+                p_lst = list(perm[j])
+                p_lst[i] = p_lst[i].swapcase()
+                perm.append(''.join(p_lst))  #-- add result to perm list
+
+    return perm    
 
 S = 'a1b2'
 
