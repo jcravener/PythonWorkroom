f85049b17b47460d9f3b5ce8d4cc4177e2ddb471
jcravener <jcravener@hotmail.com>
2020-01-28 23:50:37 -0800

    Minor code change - optimized to 65% from ~ 10%

--- GoatLatin.py
+++ GoatLatin.py
@@ -6,16 +6,15 @@ def toGoatLatin(S: str) -> str:
     if len(S) == 0:
         return None
     l = []
-    v = ["a","e","i","o","u"]
-    c = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+    v = ["a","e","i","o","u","A","E","I","O","U"]
     ct = 0
 
     for w in S.split(" "):
         ct += 1
         
-        if (w[:1]).lower() in v:
-            l.append(w+ "ma" + "a" * ct)
-        elif (w[:1]).lower() in c:
+        if w[:1] in v:
+            l.append(w + "ma" + "a" * ct)
+        else:
             l.append(w[1:] + w[:1] + "ma"+ "a" * ct)
 
     return " ".join(l)
