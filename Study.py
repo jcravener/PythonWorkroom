24be881487ff39053b357433748ef7b3e073041d
John Cravener <johncrav@microsoft.com>
2020-02-04 18:13:35 -0800

    Sort methods down

---
+++ Study.py
@@ -0,0 +1,94 @@
+import random
+
+# Differnet Functions for Study
+
+class Sort:
+    def __init__(self):
+        self
+
+    def randomList(self, r:int):
+        return [random.randrange(r) for _ in range(r)]
+
+    def quickSort(self, a:[int]):
+        if len(a) <= 1:
+            return a
+        
+        pivot = random.choice(a)
+
+        lft = [i for i in a if i < pivot]
+        eq = [i for i in a if i == pivot]
+        rt = [i for i in a if i > pivot]
+        
+        return self.quickSort(lft) + eq + self.quickSort(rt)
+    
+    def mergeSort(self, a:[int]):
+        if len(a) == 1:
+            return a
+        
+        mid = len(a)//2
+        lft = self.mergeSort(a[:mid])
+        rt = self.mergeSort(a[mid:])
+        
+        return self.__merge(lft, rt)
+    
+    def __merge(self, l:[int], r:[int]):
+        
+        tmp = []
+        li = 0
+        ri = 0
+
+        while li < len(l) and ri < len(r):
+            if l[li] < r[ri]:
+                tmp.append(l[li])
+                li += 1
+            else:
+                tmp.append(r[ri])
+                ri += 1
+        tmp += l[li:]
+        tmp += r[ri:]
+
+        return tmp
+    
+    def selectionSort(self, a:[int]):
+
+        for i in range(len(a)):
+            min = a[i]
+            mini = i
+            for j in range(i, len(a)):
+                if a[j] < min:
+                    min = a[j]
+                    mini = j
+                j += 1
+            a[i], a[mini] = a[mini], a[i]
+        
+        return a
+
+    def insertionSort(self, a:[int]):
+
+        for i in range(1, len(a)):
+            key = a[i]
+            j = i - 1
+            while j >=0 and a[j] > key:
+                a[j+1] = a[j]
+                j -= 1
+            a[j+1] = key
+        
+        return a
+
+r = 20
+srt = Sort()
+l = srt.randomList(r)
+print(l)
+print(srt.quickSort(l))
+print()
+l = srt.randomList(r)
+print(l)
+print(srt.mergeSort(l))
+print()
+l = srt.randomList(r)
+print(l)
+print(srt.selectionSort(l))
+print()
+l = srt.randomList(r)
+print(l)
+print(srt.insertionSort(l))
