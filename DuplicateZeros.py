d9cebc0dd1790b39980c870f28a5c5708156b8cc
jcravener <jcravener@hotmail.com>
2020-02-08 12:15:32 -0800

    using list methods results in change being seen

--- DuplicateZeros.py
+++ DuplicateZeros.py
@@ -14,7 +14,8 @@ class Solution:
         
         while i < alen:
             if arr[i] == 0:
-                arr = arr[:i] + tmp + arr[i:-1]
+                arr.insert(i+1, 0)
+                arr.pop()
                 i += 1
             i += 1
         
