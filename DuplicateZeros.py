957b99ab8e19004e00a584bc421172680fc4ab8a
jcravener <jcravener@hotmail.com>
2020-02-08 12:13:28 -0800

    Correct but failes because arr change is not seen

---
+++ DuplicateZeros.py
@@ -0,0 +1,26 @@
+
+
+
+# LeetCode 1089. Duplicate Zeros 
+
+class Solution:
+    def duplicateZeros(self, arr: [int]) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
+        alen = len(arr)
+        tmp =[0]
+        i = 0
+        
+        while i < alen:
+            if arr[i] == 0:
+                arr = arr[:i] + tmp + arr[i:-1]
+                i += 1
+            i += 1
+        
+        print(arr)
+
+s = Solution()
+a = [1,0,2,3,0,4,5,0]
+s.duplicateZeros(a)
+print(a)
