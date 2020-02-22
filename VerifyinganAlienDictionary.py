a4a62a4b10d24ee224a442395fd9cca4bb8110de
jcravener <jcravener@hotmail.com>
2020-02-21 23:10:30 -0800

    Still working on lexicographical rules

---
+++ VerifyinganAlienDictionary.py
@@ -0,0 +1,31 @@
+
+
+# LeetCode: 953. Verifying an Alien Dictionary
+
+def isAlienSorted(words: [str], order: str) -> bool:
+    aorder = list(order)
+    minlen = max([len(w) for w in words])
+    tmp = []
+    l = []
+
+    for i in range(minlen):
+        for w in words:
+            if i < len(w):
+                tmp.append(aorder.index(w[i]))
+            else:
+                tmp.append(-1)
+        l.append(tmp)
+        tmp = []
+
+    return l
+
+
+words = ["hello","leetcode"]
+order = "hlabcdefgijkmnopqrstuvwxyz"
+words = ["word","world","row"]
+order = "worldabcefghijkmnpqstuvxyz"
+words = ["apple","app"]
+order = "abcdefghijklmnopqrstuvwxyz"
+
+print(isAlienSorted(words, order))
+
