063309277473fe260d042ac18875898d7e87c97b
jcravener <jcravener@hotmail.com>
2020-02-21 22:05:01 -0800

    Updated comments

--- CountBinarySubstrings.py
+++ CountBinarySubstrings.py
@@ -5,7 +5,7 @@
 #  The idea here is to count up batches of characters.
 #  
 #  Once you have a full batch of both characteres, 
-#  caclulate the min value between the 2 to get the 
+#  caclulate the min len value between the 2 to get the 
 #  total substring count of those batches. E.g. 
 #  '00011' = '000' in one batch and '11' in another.
 #  The min value between these two is 2, which equals
@@ -15,7 +15,7 @@
 #  the next two by switching the current batch with
 #  the previous one and start over. You know to
 #  start over once you have switched charaters after
-#  having 2 batchesd already. When you get to this
+#  having 2 batchesd already. When you get to this point
 #  you would have seen a 3rd char i.e. '0' '1' '0'
 #  
 #  Once you're done with the entire string, you also
