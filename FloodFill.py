74ca4346987aaae5207fb488521cdb365edb1a77
jcravener <jcravener@hotmail.com>
2020-03-09 15:16:31 -0700

    working solution - fater than 47%

--- FloodFill.py
+++ FloodFill.py
@@ -5,27 +5,33 @@
 def floodFill(image: [[int]], sr: int, sc: int, newColor: int) -> [[int]]:
     if image[sr][sc] == newColor:
         return image
-        
-    def helper(image: [[int]], sr: int, sc: int, oldColor: int, newColor: int):
-        if (0 > sr >= len(image)) or (0 > sc >= len(image[0])):  #---
-            return
-        print(sr, sc)
-        image[sr][sc] = newColor
-
-        helper(image, sr+1, sc, oldColor, newColor)  
-        helper(image, sr-1, sc, oldColor, newColor)  
-        helper(image, sr, sc+1, oldColor, newColor)  
-        helper(image, sr, sc-1, oldColor, newColor)
     
-    helper(image, sr, sc, image[sr][sc], newColor)
-    return image
+    return helper(image, sr, sc, image[sr][sc], newColor)
 
+def helper(image: [[int]], sr: int, sc: int, oldColor, newColor: int) -> [[int]]:
+    if sr < 0 or sr >= len(image):
+        return image
+    if sc < 0 or sc >= len(image[0]):
+        return image
+    if image[sr][sc] != oldColor:
+        return image
+    
+    image[sr][sc] = newColor
+    
+    image = helper(image, sr+1, sc, oldColor, newColor)
+    image = helper(image, sr-1, sc, oldColor, newColor)
+    image = helper(image, sr, sc+1, oldColor, newColor)
+    image = helper(image, sr, sc-1, oldColor, newColor)
+
+    return image
 
 img = [[1,1,1],[1,1,0],[1,0,1]]
 sr = 1
 sc = 1
 newcolor = 2
 
+for r in img:
+    print(r)
+print()
 for r in floodFill(img, sr, sc, newcolor):
     print(r)
-    
