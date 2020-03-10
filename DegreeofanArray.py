733328a2c9e9bff5154d36da889a105e68e9fdc4
jcravener <jcravener@hotmail.com>
2020-03-09 22:44:27 -0700

    accepted but slow: 12.89%

--- DegreeofanArray.py
+++ DegreeofanArray.py
@@ -4,6 +4,8 @@
 
 def findShortestSubArray(nums: [int]) -> int:
     d = {}
+    l = []
+    numsrev = nums[::-1]
 
     for n in nums:
         if n not in d:
@@ -11,7 +13,15 @@ def findShortestSubArray(nums: [int]) -> int:
         else:
             d[n] += 1
     
-    return d
+    mxval = max(d, key=lambda k: d[k])
+    deg = d[mxval]
+
+    for k, v in d.items():
+        if v == deg:
+            l.append(len(nums) - numsrev.index(k) - nums.index(k))  #--- this is my on invention :).  It finds the length of the subarray by calulating the distance between the first degree val on the left with the last degree val on the right.
+    
+    return min(l)
 
 inpt = [1,2,2,3,1,4,2]
+inpt = [1, 2, 2, 3, 1]
 print(findShortestSubArray(inpt))
